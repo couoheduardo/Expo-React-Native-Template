1fba77dd7d9d49eff07fd207fc2c951aac8ed84e
@couoheduardo <couoheduardo@icloud.com>
2024-01-26 16:59:30 -0600

    Update App.js

--- App.js
+++ App.js
@@ -29,3 +29,36 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {}
 });
+
+
+
+
+
+
+
+
+
+
+import axios from "axios";
+import Constants from 'expo-constants';
+
+const axiosInstance = axios.create({
+    baseURL: Constants.manifest.extra.production ? Constants.manifest.extra.URL : Constants.manifest.extra.URL_DEV,
+    headers: {
+        Accept: "application/json",
+        Platform: 'app-partner'
+    }
+});
+axiosInstance.interceptors.request.use(async (request) => {
+    return request;
+}, (error) => {
+    return Promise.reject(error.response)
+})
+
+axiosInstance.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    return Promise.reject(error.response)
+})
+
+export default axiosInstance;
