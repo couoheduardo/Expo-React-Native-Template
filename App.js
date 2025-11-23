b7e8b9b4d140a211500399723a236fc3cf11331a
Eduardo Couoh <couoheduardo@icloud.com>
2025-11-22 23:29:34 -0600

    upgrade to expo 54

--- App.js
+++ App.js
@@ -1,64 +1,20 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {NativeBaseProvider, Box, Text} from 'native-base';
-import {theme} from "./theme";
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from "@react-navigation/native";
-import HomeScreen from "./screens/HomeScreen";
-import {Provider} from "react-redux";
-import {store} from "./redux/store";
-
-const Stack = createStackNavigator();
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return (
-        <Provider store={store}>
-        <NativeBaseProvider theme={theme}>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="Home" component={HomeScreen}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-        </NativeBaseProvider>
-        </Provider>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {}
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
-
-
-
-
-
-
-
-
-
-
-import axios from "axios";
-import Constants from 'expo-constants';
-
-const axiosInstance = axios.create({
-    baseURL: Constants.manifest.extra.production ? Constants.manifest.extra.URL : Constants.manifest.extra.URL_DEV,
-    headers: {
-        Accept: "application/json",
-        Platform: 'app-partner'
-    }
-});
-axiosInstance.interceptors.request.use(async (request) => {
-    return request;
-}, (error) => {
-    return Promise.reject(error.response)
-})
-
-axiosInstance.interceptors.response.use((response) => {
-    return response;
-}, (error) => {
-    return Promise.reject(error.response)
-})
-
-export default axiosInstance;
